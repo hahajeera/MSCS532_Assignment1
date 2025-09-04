3eb3d3439281d1d34e3820b16f63a0ea84623494
hahajeera <hhajeera39484@ucumberlands.edu>
2025-09-03 19:09:01 -0500

    Create insertion_sort.py

---
+++ insertion_sort.py
@@ -0,0 +1,10 @@
+def insertion_sort_desc(arr):
+    """
+    This will eventually sort the array in descending order.
+    Not implemented yet.
+    """
+    raise NotImplementedError("Not yet implemented")
+
+
+if __name__ == "__main__":
+    print("Placeholder script for insertion sort.")
